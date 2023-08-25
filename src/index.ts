6a5ca0759511972fe6678dee18a5aacb4f05c107
marcopolo1124 <85742840+marcopolo1124@users.noreply.github.com>
2023-08-25 17:07:34 +0100

    delete .env

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
 import articleRouter from "./routes/articles";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const MONGO_DB_URL = process.env.MONGO_DB_URL || "mongodb://localhost/blog";
 
-mongoose.connect("mongodb://localhost/blog");
+mongoose.connect(MONGO_DB_URL);
 
 app.get("/", (req, res, next) => {
     res.send({

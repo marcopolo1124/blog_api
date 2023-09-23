52535e7f3c640baa368a59756ebff8dbc9f26eac
marcopolo1124 <85742840+marcopolo1124@users.noreply.github.com>
2023-09-23 15:31:37 +0100

    cors fix

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,4 +1,4 @@
 export const origin: Record<string, string> = {
     development: "http://localhost:5173",
-    production: "https://markdown-blog-ygxm.onrender.com/"
+    production: "https://markdown-blog-ygxm.onrender.com"
 };
